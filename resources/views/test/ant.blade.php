624c9289e4ac72547c3a6e5d54ffff243b66cdf2
Music62 <towyoumiw@gmail.com>
2023-06-27 17:25:46 +0700

    nice

---
+++ resources/views/test/ant.blade.php
@@ -0,0 +1,14 @@
+<!-- View stored in resources/views/gallery/ant.blade.php -->
+<html>
+
+<body>
+
+    <h2>Phaninthorn Nongtakrai</h2>
+    <a href="gallery/ant" >
+    <img src=" {{ $ant }} "Trulli width="500" height="250">
+    </a>
+    
+
+</body>
+
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,16 @@
+<!-- View stored in resources/views/gallery.blade.php -->
+<html>
+
+<body>
+
+    <h2>Phaninthorn Nongtakrai</h2>
+    <img src=" {{ $ant }} "Trulli width="500" height="250">
+    <img src=" {{ $bird }} "Trulli width="500" height="250">
+    <img src=" {{ $cat }} "Trulli width="500" height="250">
+    <img src=" {{ $god }} "Trulli width="500" height="250">
+    <img src=" {{ $spider }} "Trulli width="500" height="250">
+
+
+</body>
+
+</html>
